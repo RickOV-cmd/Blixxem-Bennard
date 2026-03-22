e0d05418a46922813f86f1597be660301e6eb089
RickOV-cmd <rickoldevelthuis@gmail.com>
2026-03-22 18:00:05 +0100

    Delete blixxem-bennard-1to1 directory

--- blixxem-bennard-1to1/js/privacy-strip.js
+++
@@ -1,7 +0,0 @@
-// Show privacy strip only once per browser session (not on every visit)
-  (function(){
-    if(!localStorage.getItem('bb_privacy_noted')){
-      const s=document.getElementById('privacyStrip');
-      if(s) s.style.display='flex';
-    }
-  })();
